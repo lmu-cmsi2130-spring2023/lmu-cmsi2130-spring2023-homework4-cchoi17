023ce8c13ebf2113f4a7fa4473af891a8f46cf64
christina choi <cchoi17@lion.lmu.edu>
2023-04-13 21:03:02 -0700

    Finished Homework

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 # CMSI 2130 - Homework 4
 If at first your tests don't pass, trie trie again
 
-** PLACE NAME HERE **
+** Christina Choi **
--- src/main/compression/Huffman.java
+++ src/main/compression/Huffman.java
@@ -1,7 +1,9 @@
 package main.compression;
 
 import java.util.*;
-import java.io.ByteArrayOutputStream; // Optional
+import java.util.Arrays;
+import org.w3c.dom.Node;
+import java.io.*;
 
 /**
  * Huffman instances provide reusable Huffman Encoding Maps for
@@ -9,7 +11,7 @@ import java.io.ByteArrayOutputStream; // Optional
  * distributions of characters.
  */
 public class Huffman {
-    
+
     // -----------------------------------------------
     // Construction
     // -----------------------------------------------
@@ -19,27 +21,75 @@ public class Huffman {
     private TreeMap<Character, String> encodingMap;
     // Character that represents the end of a compressed transmission
     private static final char ETB_CHAR = 23;
-    
+
     /**
+     * 
      * Creates the Huffman Trie and Encoding Map using the character
      * distributions in the given text corpus
      * 
      * @param corpus A String representing a message / document corpus
-     *        with distributions over characters that are implicitly used
-     *        throughout the methods that follow. Note: this corpus ONLY
-     *        establishes the Encoding Map; later compressed corpi may
-     *        differ.
+     *               with distributions over characters that are implicitly used
+     *               throughout the methods that follow. Note: this corpus ONLY
+     *               establishes the Encoding Map; later compressed corpi may
+     *               differ.
      */
-    public Huffman (String corpus) {
-        // TODO!
-        throw new UnsupportedOperationException();
+    public Huffman(String corpus) {
+        HashMap<Character, Integer> distribution = new HashMap<>();
+        distribution.put(ETB_CHAR, 1);
+        for (int i = 0; i < corpus.length(); i++) {
+            if (distribution.containsKey(corpus.charAt(i))) {
+                int value = distribution.get(corpus.charAt(i)) + 1;
+                distribution.replace(corpus.charAt(i), value);
+            } else {
+                distribution.put(corpus.charAt(i), 1);
+            }
+        }
+        PriorityQueue<HuffNode> huffmanTrie = new PriorityQueue<>();
+        for (Map.Entry<Character, Integer> entry : distribution.entrySet()) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            HuffNode leaf = new HuffNode(key, value);
+            huffmanTrie.add(leaf);
+        }
+        while (huffmanTrie.size() != 1) {
+            HuffNode currZeroChild = huffmanTrie.poll();
+            HuffNode currOneChild = huffmanTrie.poll();
+            HuffNode parent = new HuffNode(currZeroChild.character, 0);
+            parent.zeroChild = currZeroChild;
+            parent.oneChild = currOneChild;
+            parent.count = currZeroChild.count + currOneChild.count;
+            huffmanTrie.add(parent);
+        }
+        trieRoot = huffmanTrie.poll();
+        // huffmanTrie.add(trieRoot);
+        this.encodingMap = new TreeMap<>();
+        toBitCode(trieRoot, "");
+        System.out.println(encodingMap);
     }
-    
-    
+
+    /**
+     * Converting the Binary tree of HuffNodes with characters and their count into
+     * the
+     * encoding map of their bitcode.
+     * 
+     * @param huffnode. The current huffnode we're in.
+     *                  bitCode. The current bitcode
+     * @return treemap. the map of each character and its bitcode
+     */
+    public TreeMap<Character, String> toBitCode(HuffNode current, String bitCode) {
+        if (current.isLeaf()) {
+            encodingMap.put(current.character, bitCode);
+            return encodingMap;
+        }
+        toBitCode(current.zeroChild, bitCode + "0");
+        toBitCode(current.oneChild, bitCode + "1");
+        return encodingMap;
+    }
+
     // -----------------------------------------------
     // Compression
     // -----------------------------------------------
-    
+
     /**
      * Compresses the given String message / text corpus into its Huffman coded
      * bitstring, as represented by an array of bytes. Uses the encodingMap
@@ -51,64 +101,115 @@ public class Huffman {
      *         (1) the bitstring containing the message itself, (2) possible
      *         0-padding on the final byte.
      */
-    public byte[] compress (String message) {
-        // TODO!
-        throw new UnsupportedOperationException();
+    public byte[] compress(String message) {
+        String result = "";
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        for (int i = 0; i < message.length(); i++) {
+            String current = encodingMap.get(message.charAt(i));
+            result += current;
+        }
+        result += encodingMap.get(ETB_CHAR);
+        while (result.length() % 8 != 0) {
+            result += "0";
+        }
+        while (result.length() != 0) {
+            String eightBits = result.substring(0, 8);
+            output.write((byte) Integer.parseInt(eightBits, 2));
+            result = result.substring(8);
+        }
+        return output.toByteArray();
     }
-    
-    
+
     // -----------------------------------------------
     // Decompression
     // -----------------------------------------------
-    
+
     /**
      * Decompresses the given compressed array of bytes into their original,
      * String representation. Uses the trieRoot field (the Huffman Trie) that
      * generated the compressed message during decoding.
      * 
-     * @param compressedMsg {@code byte[]} representing the compressed corpus with the
-     *        Huffman coded bytecode. Formatted as:
-     *        (1) the bitstring containing the message itself, (2) possible
-     *        0-padding on the final byte.
-     * @return Decompressed String representation of the compressed bytecode message.
+     * @param compressedMsg {@code byte[]} representing the compressed corpus with
+     *                      the
+     *                      Huffman coded bytecode. Formatted as:
+     *                      (1) the bitstring containing the message itself, (2)
+     *                      possible
+     *                      0-padding on the final byte.
+     * @return Decompressed String representation of the compressed bytecode
+     *         message.
      */
-    public String decompress (byte[] compressedMsg) {
-        // TODO!
-        throw new UnsupportedOperationException();
+    public String decompress(byte[] compressedMsg) {
+        String wholeString = "";
+        for (int i = 0; i < compressedMsg.length; i++) {
+            String binString = String.format("%8s", Integer.toBinaryString(compressedMsg[i] & 0xff)).replace(" ", "0");
+            wholeString += binString;
+        }
+        return generateMessage(trieRoot, 0, wholeString, "");
     }
-    
-    
+
+    /**
+     * Decompresses the given compressed array of bytes into their original,
+     * String representation. Uses the trieRoot field (the Huffman Trie) that
+     * generated the compressed message during decoding.
+     * 
+     * @param compressedMsg {@code byte[]} representing the compressed corpus with
+     *                      the
+     *                      Huffman coded bytecode. Formatted as:
+     *                      (1) the bitstring containing the message itself, (2)
+     *                      possible
+     *                      0-padding on the final byte.
+     * @return Decompressed String representation of the compressed bytecode
+     *         message.
+     */
+    private String generateMessage(HuffNode currNode, int currIndex, String wholeString, String decompressedMsg) {
+        if (currNode.isLeaf()) {
+            if (currNode.character == ETB_CHAR) {
+                return decompressedMsg;
+            }
+            decompressedMsg += currNode.character;
+            currNode = trieRoot;
+        }
+        if (wholeString.charAt(currIndex) == '0') {
+            return generateMessage(currNode.zeroChild, currIndex + 1, wholeString, decompressedMsg);
+        } else {
+            return generateMessage(currNode.oneChild, currIndex + 1, wholeString, decompressedMsg);
+        }
+    }
+
     // -----------------------------------------------
     // Huffman Trie
     // -----------------------------------------------
-    
+
     /**
      * Huffman Trie Node class used in construction of the Huffman Trie.
-     * Each node is a binary (having at most a left (0) and right (1) child), contains
-     * a character field that it represents, and a count field that holds the 
-     * number of times the node's character (or those in its subtrees) appear 
+     * Each node is a binary (having at most a left (0) and right (1) child),
+     * contains
+     * a character field that it represents, and a count field that holds the
+     * number of times the node's character (or those in its subtrees) appear
      * in the corpus.
      */
     private static class HuffNode implements Comparable<HuffNode> {
-        
+
         HuffNode zeroChild, oneChild;
         char character;
         int count;
-        
-        HuffNode (char character, int count) {
+
+        HuffNode(char character, int count) {
             this.count = count;
             this.character = character;
         }
-        
-        public boolean isLeaf () {
+
+        public boolean isLeaf() {
             return this.zeroChild == null && this.oneChild == null;
         }
-        
-        public int compareTo (HuffNode other) {
-            // TODO: Implemented incorrectly at the moment!
-            return 0;
+
+        public int compareTo(HuffNode other) {
+            if (this.count == other.count) {
+                return this.character - other.character;
+            }
+            return this.count - other.count;
         }
-        
+
     }
 
 }
